f9cf8ac403b2a53e0908647ea555cc6e784ea03d
TonystarkMark50 <sasikalasasikala047@gmail.com>
2026-07-06 08:30:45 +0530

    Initial commit

---
+++ studio.py
@@ -0,0 +1,32 @@
+﻿print("=" * 50)
+print("        BioVerse OS Studio v0.1")
+print("=" * 50)
+
+print("""
+1. Create Desktop
+2. Create Launcher
+3. Create Dock
+4. Create File Manager
+5. Build ISO
+6. Exit
+""")
+
+choice = input("Select an option: ")
+
+if choice == "1":
+    print("Desktop module will be created in the next version.")
+
+elif choice == "2":
+    print("Launcher module will be created in the next version.")
+
+elif choice == "3":
+    print("Dock module will be created in the next version.")
+
+elif choice == "4":
+    print("File Manager module will be created in the next version.")
+
+elif choice == "5":
+    print("ISO Builder will be created in the next version.")
+
+else:
+    print("Goodbye!")
